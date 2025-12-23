d591cdc452ce0b5e83421e80f28ae70a71b02178
Alghazali <118599868+alee0214@users.noreply.github.com>
2025-12-24 00:24:10 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -25,7 +25,7 @@ def train_model():
     )
 
     # MLflow local tracking
-    mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+    # mlflow.set_tracking_uri("http://127.0.0.1:5000/")
     mlflow.set_experiment("Credit Card Fraud Detection")
 
     # Enable autolog
@@ -59,4 +59,5 @@ def train_model():
 
 if __name__ == "__main__":
     train_model()
+
 
